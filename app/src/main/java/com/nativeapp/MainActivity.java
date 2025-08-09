0f1e7a320e0d7e4eae2ce3232a37a2ca6b716fd7
mahdi66rsh-cmd <mahdi66rsh@gmail.com>
2025-08-09 20:13:02 +0330

    Update MainActivity.java

--- app/src/main/java/com/nativeapp/MainActivity.java
+++ app/src/main/java/com/nativeapp/MainActivity.java
@@ -1,23 +1,12 @@
 package com.example.nativeapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    static {
-        System.loadLibrary("core"); // اسم کتابخانه C++ = libcore.so
-    }
-
-    public native String helloFromCpp();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(helloFromCpp());
     }
 }
