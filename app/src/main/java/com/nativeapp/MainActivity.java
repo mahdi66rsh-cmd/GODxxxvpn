0b9998d75eac3389fdb4b4cfe3ded9b89a10d868
mahdi66rsh-cmd <mahdi66rsh@gmail.com>
2025-08-09 20:34:19 +0330

    Update MainActivity.java

--- app/src/main/java/com/nativeapp/MainActivity.java
+++ app/src/main/java/com/nativeapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.nativeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btnNext);
+        btn.setOnClickListener(v ->
+                startActivity(new Intent(this, SecondActivity.class)));
     }
 }
